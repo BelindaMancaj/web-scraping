35cb33eb8f8a67532e465af69cec2ea3148b2d0a
Belinda Mancaj <bmancaj13@gmail.com>
2023-11-08 21:47:16 +0100

    Some versions

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-const cheerio = require("cheerio")
-const axios = require("axios")
 
-async function performScraping() {
-    // downloading the target web page
-    // by performing an HTTP GET request in Axios
+const cheerio = require('cheerio');
+const axios = require('axios');
+ 
+const performScraping = async () => {
+  const axiosResponse = await axios.request({
+    method: 'GET',
+    url: 'https://www.century21albania.com/properties'
+  });
+ 
+  const $ = cheerio.load(axiosResponse.data);
+ 
+  const maxPages = $('.pagination li:nth-last-child(2)').find('a').text();
+ 
+  const links = await getAllPropertyLinks(2);
+ 
+  console.log(links);
+};
+ 
+performScraping();
+ 
+const getAllPropertyLinks = async (pages) => {
+  const links = [];
+ 
+  for (let i = 1; i <= pages; i++) {
     const axiosResponse = await axios.request({
-        method: "GET",
-        url: "https://www.century21albania.com/properties",
-        headers: {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-        }
-    })
-
-    const $ = cheerio.load(axiosResponse.data)
-    const houses = [];
-
-   $(".osahan_top_filter").find(".property").each((index, element) => {
-        
-        const title = $(element).find(".card-body").find(".card-title").text()
-        const price = $(element).find(".card-body").find(".mb-2").text().trim()
-        const measures = $(element).find(".card-footer").find(".FutureInfo").text().trim()
-        
-        const house = {
-            title: title,
-            price: price
-        }
-        
-        houses.push(measures)
-    })
-    console.log(houses)
-}
-
-performScraping()
+      method: 'GET',
+      url: `https://www.century21albania.com/properties?page=${i}`
+    });
+ 
+    const $ = cheerio.load(axiosResponse.data);
+ 
+    $('.osahan_top_filter')
+      .find('.property')
+      .each((index, element) => {
+        const link = $(element).find('a:eq(1)').attr('href').each((i, info)=>{
+            const houseInfo = $('.card-body').find("p").text()
+            links.push(houseInfo);
+        }); //.text();
+ 
+      });
+  }
+ 
+  return links;
+};
